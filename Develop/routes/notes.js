1805dee4145cca1f60fcb042509bc3a2709ed178
Vincent Baraga <vincentb.baraga@gmail.com>
2023-11-05 16:55:05 -0500

    added post and get functionality for the /api/notes route

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -1,2 +1,34 @@
 const notes = require("express").Router();
 const { v4: uuidv4 } = require("uuid");
+const fs = require("fs");
+const util = require("util");
+const readFromFile = util.promisify(fs.readFile);
+
+notes.get("/notes", (req, res) => {
+  readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
+});
+
+notes.post("/notes", (req, res) => {
+  console.log(req.body);
+
+  const { title, text } = req.body;
+
+  if (req.body) {
+    const newNote = { title, text, noteId: uuidv4() };
+    console.log(newNote);
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
+      if (err) {
+        console.log(err);
+      } else {
+        const parsedData = JSON.parse(data);
+        parsedData.push(newNote);
+        fs.writeFile(
+          "./db/db.json",
+          JSON.stringify(parsedData, null, 4),
+          (err) => (err ? console.error(err) : console.info("Note Saved"))
+        );
+      }
+    });
+  }
+});
+module.exports = notes;
--- Develop/server.js
+++ Develop/server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const path = require("path");
+const api = require("./routes/notes");
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use("/api", api);
 app.use(express.static("public"));
 
 app.get("/", (req, res) =>
