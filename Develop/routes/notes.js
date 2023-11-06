e3e79ea1e52228679412bed4ac96a0c597283804
Vincent Baraga <vincentb.baraga@gmail.com>
2023-11-06 13:32:51 -0500

    added the functionality to delete and view specific notes

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -2,10 +2,37 @@ const notes = require("express").Router();
 const { v4: uuidv4 } = require("uuid");
 const fs = require("fs");
 const util = require("util");
+const { json } = require("express");
 const readFromFile = util.promisify(fs.readFile);
 
 notes.get("/notes", (req, res) => {
   readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
+});
+
+notes.get("/notes/:id", (req, res) => {
+  const noteID = req.params.id;
+  readFromFile("./db/db.json")
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      const result = json.filter((note) => note.id === noteID);
+      return result.length > 0
+        ? res.json(result)
+        : res.json("No note with that id found");
+    });
+});
+
+notes.delete("/notes/:id", (req, res) => {
+  const noteID = req.params.id;
+  readFromFile("./db/db.json")
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      const result = json.filter((note) => note.id !== noteID);
+      console.info(result);
+      fs.writeFile("./db/db.json", JSON.stringify(result, null, 4), (err) =>
+        err ? console.error(err) : console.info("No errors.")
+      );
+      res.json("deleted note successfull");
+    });
 });
 
 notes.post("/notes", (req, res) => {
@@ -14,7 +41,7 @@ notes.post("/notes", (req, res) => {
   const { title, text } = req.body;
 
   if (req.body) {
-    const newNote = { title, text, noteId: uuidv4() };
+    const newNote = { title, text, id: uuidv4() };
     console.log(newNote);
     fs.readFile("./db/db.json", "utf8", (err, data) => {
       if (err) {
@@ -25,8 +52,9 @@ notes.post("/notes", (req, res) => {
         fs.writeFile(
           "./db/db.json",
           JSON.stringify(parsedData, null, 4),
-          (err) => (err ? console.error(err) : console.info("Note Saved"))
+          (err) => (err ? console.error(err) : console.info("No errors."))
         );
+        res.json("Note saved");
       }
     });
   }
